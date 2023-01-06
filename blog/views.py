345b55c14e2a51c23d76e3129f017de1acc19515
matrix-bro <razmhz11@gmail.com>
2023-01-06 12:00:16 +0545

    Added dummy data and displayed it on home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author' : 'Matrix',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'Jan6 2023'
+    },
+    {
+        'author' : 'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'Jan7 2023'
+    }
+]
+
 # Create your views here.
 
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
