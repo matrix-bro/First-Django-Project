37e4d862f87300e63bfa81f16a03ce47f58c9654
matrix-bro <razmhz11@gmail.com>
2023-01-07 16:20:33 +0545

    Created Post table and added data and displayed it on home page

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author' : 'Matrix',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'Jan6 2023'
-    },
-    {
-        'author' : 'Jane Doe',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'Jan7 2023'
-    }
-]
+# posts = [
+#     {
+#         'author' : 'Matrix',
+#         'title' : 'Blog Post 1',
+#         'content' : 'First post content',
+#         'date_posted' : 'Jan6 2023'
+#     },
+#     {
+#         'author' : 'Jane Doe',
+#         'title' : 'Blog Post 2',
+#         'content' : 'Second post content',
+#         'date_posted' : 'Jan7 2023'
+#     }
+# ]
 
 # Create your views here.
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
