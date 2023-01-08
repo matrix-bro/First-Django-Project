fd3a825b35d0546d1fa44acdaacc549c57d2f95a
matrix-bro <razmhz11@gmail.com>
2023-01-08 14:12:48 +0545

    Registered users app in settings.py file

--- first_django_project/settings.py
+++ first_django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',  #need to register newly created apps here
+    'users.app.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
