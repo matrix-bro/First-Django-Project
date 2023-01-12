94dea2ad223813fab593a438274c844c7f99ab71
matrix-bro <razmhz11@gmail.com>
2023-01-12 14:03:55 +0545

    Created class based view called list view for Post model

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-# posts = [
-#     {
-#         'author' : 'Matrix',
-#         'title' : 'Blog Post 1',
-#         'content' : 'First post content',
-#         'date_posted' : 'Jan6 2023'
-#     },
-#     {
-#         'author' : 'Jane Doe',
-#         'title' : 'Blog Post 2',
-#         'content' : 'Second post content',
-#         'date_posted' : 'Jan7 2023'
-#     }
-# ]
-
-# Create your views here.
-
+#Function based views
 def home(request):
     context = {
         'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
+
+#Class based views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
