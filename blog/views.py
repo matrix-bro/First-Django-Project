45791a47eb9c8e56905d495aaa3112fec8685e58
matrix-bro <razmhz11@gmail.com>
2023-01-13 12:23:27 +0545

    Created ListView to display all posts created by user

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 #Function based views
 def home(request):
@@ -18,6 +19,15 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 5
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html  Eg, blog/home_list.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post   
